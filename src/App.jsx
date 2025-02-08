9dcdea728c4380f2b7c514d6f8ac9b99364ab97c
CQSTechSolutions <cqstechsolutionsofficial@gmail.com>
2025-02-08 11:22:11 +0530

    testimonials

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import HeaderSection from "./sections/HeaderSection";
+import TestimonialsSection from "./sections/TestimonialsSection";
+// import AboutSection from "./sections/AboutSection";
 
 function App() {
 
   return (
     <div>
       <HeaderSection />
+      <TestimonialsSection />
     </div>
   )
 }
