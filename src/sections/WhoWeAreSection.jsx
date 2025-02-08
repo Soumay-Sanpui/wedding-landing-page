f672e2c79b314cbd7f113ae9bbb5efbf0496f8b1
Soumay-sanpui <somusanpui9@gmail.com>
2025-02-08 12:06:27 +0530

    re: whoweare

--- src/sections/WhoWeAreSection.jsx
+++ src/sections/WhoWeAreSection.jsx
@@ -1,10 +1,10 @@
 const WhoWeAreSection = () => {
     return(
-        <section className={"w-screen h-screen p-4 flex flex-col gap-4  items-center justify-center"}>
+        <section className={"w-screen h-screen bg-[url('/wwa.jpeg')] bg-cover bg-fit bg-no-repeat bg-fixed text-white p-4 flex flex-col gap-4  items-center justify-center"}>
             {/* Section title */}
             <h1 className={"text-pink-500 font-bold text-xl"}>Who We Are</h1>
             {/* Section content */}
-            <p className={"text-center border mx-22"}>Storytelling is our core. When we are not shooting weddings, we are making movies, documentaries, and web series. Weddings help us generate capital for our film projects without giving up our company's equity to investors. We generate revenue from these movies since we align the distribution of our films in advance. Hence, we're able to offer not just incredible wedding memories but also an opportunity to earn from it.</p>
+            <p className={"text-center mx-22"}>Storytelling is our core. When we are not shooting weddings, we are making movies, documentaries, and web series. Weddings help us generate capital for our film projects without giving up our company's equity to investors. We generate revenue from these movies since we align the distribution of our films in advance. Hence, we're able to offer not just incredible wedding memories but also an opportunity to earn from it.</p>
         </section>
     )
 }
