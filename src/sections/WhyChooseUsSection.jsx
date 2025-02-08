63dd33d084a50639c37caa2f6c5ca77a059cbf20
Soumay-sanpui <somusanpui9@gmail.com>
2025-02-08 12:46:14 +0530

    re: cosmetics V2

--- src/sections/WhyChooseUsSection.jsx
+++ src/sections/WhyChooseUsSection.jsx
@@ -24,7 +24,7 @@ const WhyChooseUsSection = () => {
                 <h2 className="text-center font-cormorant text-4xl mb-16 uppercase text-pink-500 font-bold">Why You Should Consider This?</h2>
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
                     {features.map((feature, index) => (
-                        <div key={index} className="text-center py-8 px-4 border cursor-pointer border-pink-500/30 hover:border-pink-500 transition-colors rounded-lg shadow-lg bg-white bg-opacity-10 backdrop-blur-md flex flex-col justify-between">
+                        <div key={index} className="text-center py-8 px-4 border cursor-pointer border-pink-500/30 hover:border-pink-500 transition-colors rounded-sm shadow-lg bg-white bg-opacity-10 backdrop-blur-md flex flex-col justify-between">
                             <h3 className="text-xl font-cormorant mb-2 text-pink-500">{feature.title}</h3>
                             <p className="text-gray-700 text-sm">{feature.description}</p>
                         </div>
