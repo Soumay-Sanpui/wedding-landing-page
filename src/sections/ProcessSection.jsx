d1f11bf1ef6b96e8a1d56ff2ce63f016fe53e40c
Soumay-sanpui <somusanpui9@gmail.com>
2025-02-08 13:58:16 +0530

    cosmetics v3

--- src/sections/ProcessSection.jsx
+++ src/sections/ProcessSection.jsx
@@ -1,7 +1,7 @@
 const ProcessSection = () => {
     return (
-        <section className="py-8 md:py-16 lg:py-20 flex flex-col items-center justify-center border bg-black">
-            <h2 className="text-center text-2xl lg:text-5xl font-bold uppercase mb-6 md:mb-10 text-white">The Process</h2>
+        <section className="py-8 md:py-16 lg:py-20 flex flex-col items-center justify-center bg-[#292929]">
+            <h2 className="text-center text-2xl lg:text-5xl font-bold uppercase mb-6 md:mb-10 text-pink-500">The Process</h2>
             <div className={"flex items-center justify-center"}>
                 <div className="flex flex-col items-start justify-center w-full px-4 md:px-8 lg:px-16">
                     <div className="my-2 md:my-4 flex flex-col md:flex-row items-center justify-center gap-3 md:gap-5">
@@ -20,9 +20,9 @@ const ProcessSection = () => {
             {/* Plan Section */}
             <div className="w-full flex flex-col md:flex-row items-center md:items-start justify-center gap-6 md:gap-8 mt-8 px-4 md:px-8">
                 {/* Plan A */}
-                <div className="w-full md:w-[45vw] lg:w-[40vw] h-auto rounded-md bg-pink-500 text-black p-4 md:p-6 lg:p-8">
+                <div className="text-white w-full md:w-[45vw] lg:w-[40vw] h-auto rounded-md bg-pink-500 text-black p-4 md:p-6 lg:p-8">
                     <div className="w-full mb-4">
-                        <h3 className="font-bold text-xl md:text-2xl lg:text-3xl text-black text-center">Plan A - Assured Returns</h3>
+                        <h3 className="text-white font-bold text-xl md:text-2xl lg:text-3xl text-black text-center">Plan A - Assured Returns</h3>
                     </div>
                     <div className="w-full">
                         <ul className="list-disc space-y-4 md:space-y-6 text-base md:text-lg lg:text-xl ml-4 md:ml-6">
@@ -34,9 +34,9 @@ const ProcessSection = () => {
                 </div>
 
                 {/* Plan B */}
-                <div className="w-full md:w-[45vw] lg:w-[40vw] h-auto rounded-md bg-pink-500 text-black p-4 md:p-6 lg:p-8">
+                <div className="text-white w-full md:w-[45vw] lg:w-[40vw] h-auto rounded-md bg-pink-500 text-black p-4 md:p-6 lg:p-8">
                     <div className="w-full mb-4">
-                        <h3 className="font-bold text-xl md:text-2xl lg:text-3xl text-black text-center">Plan B - Investment Returns</h3>
+                        <h3 className="text-white font-bold text-xl md:text-2xl lg:text-3xl text-black text-center">Plan B - Investment Returns</h3>
                     </div>
                     <div className="w-full">
                         <ul className="list-disc space-y-4 md:space-y-6 text-base md:text-lg lg:text-xl ml-4 md:ml-6">
